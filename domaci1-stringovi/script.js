271a14563818589ae081db41d1d4c184973cac3f
Hana Plojovic <hanaplojovic123@gmail.com>
2023-01-20 15:38:13 +0100

    JS-course

--- domaci1-stringovi/script.js
+++ domaci1-stringovi/script.js
@@ -1,35 +1,3 @@
-// function najduzaRec(string) {
-//     var str = string.split(" ");
-//     var longest = 0;
-//     var najduzarec;
-//     str.forEach(function(str) {
-//         if (longest < str.length) {
-//             longest = str.length;
-//             najduzarec = str;
-//         }
-//     });
-//     return najduzarec;
-// }
-// console.log("recenica :   Ja volim programiranje")
-// console.log("Najduza rec u recenici je:  ",najduzaRec("Ja volim programiranje"));
-
-// function getLongestWord(str){
-//     let words = str.split(' ');
-//     let maxLength = 0;
-//     let longestWord = '';
-  
-//     for (let i = 0; i < words.length; i++) {
-//       if (words[i].length > maxLength) {
-//         maxLength = words[i].length;
-//         longestWord = words[i];
-//       }
-//     }
-//     console.log(longestWord)
-  
-//   }
-// getLongestWord("Ja volim programiranje")
-
-
 str = "Ja volim programiranje";
     var reci = str.split(' ');
     var maxLength = 0;
