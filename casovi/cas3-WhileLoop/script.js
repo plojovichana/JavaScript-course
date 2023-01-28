239958a1b0c1b06411b197c371135fca51c441cc
Hana Plojovic <hanaplojovic123@gmail.com>
2023-01-28 15:10:54 +0100

    JS-course

---
+++ casovi/cas3-WhileLoop/script.js
@@ -0,0 +1,67 @@
+// let n = 10;
+// while(n>0){
+//     console.log(n);
+//     n--;
+// }
+
+
+
+// let n = 1;
+
+// while(n<11){
+//     console.log(n);
+//     n++;
+// }
+
+
+// let n = 1;
+// while(true){
+//     console.log(n);
+//     n++
+// }
+
+
+
+// n = 1;
+// do{
+//     console.log(n);
+//     n++;
+// }
+// while(n<10);
+
+//1.
+
+// function provera(str){
+//     if (str.length % 2 === 0){
+//         return (str.slice(0,str.length/2) +'|' + str.slice(str.length/2,str.length));
+//     }
+//     else{
+//         return (str.slice(0,str.length/2) +'|' + str.slice((str.length/2 + 1), str.length));
+//     }
+// }
+// console.log(provera("abcd"));
+
+//2.
+
+//const a = "hello world";
+// console.log(a.split("").join("."));
+
+// function separator (str,sep){
+//     return(str.split("").join(sep));
+// }
+// console.log(separator("Hello World","."))
+
+//3.
+
+// n = 5;
+// proizvod = 1;
+
+// function faktorijal (br){
+//     while(n>0){
+//     proizvod = proizvod*n;
+//     n--;
+// }
+// return proizvod;
+// }
+
+// console.log(faktorijal(n));
