950205e2712966e4f6a8b678098ddc930331547e
Hana Plojovic <hanaplojovic123@gmail.com>
2023-01-28 15:45:33 +0100

    JS-course

--- casovi/cas3-WhileLoop/script.js
+++ casovi/cas3-WhileLoop/script.js
@@ -53,15 +53,47 @@
 
 //3.
 
-n = 5;
-let proizvod = 1;
+// n = 5;
+// let proizvod = 1;
 
-function faktorijal (br){
-    while(br>0){
-    proizvod *= br;
-    br--;
-}
-return proizvod;
-}
+// function faktorijal (br){
+//     while(br>0){
+//     proizvod *= br;
+//     br--;
+// }
+// return proizvod;
+// }
 
-console.log(faktorijal(n));
+// console.log(faktorijal(n));
+
+//4.
+
+// n = 20;
+// suma = 0;
+// function sabiranje(br){
+//     while(br>0){
+//         if(br % 2 === 0 ){
+//             suma += br;
+//         }
+//     }
+// }
+// console.log(sabiranje(n));
+
+//5.
+
+// let niz = [1,2,3,4,5,6];
+// let spojBrojeve= niz.reduce((prethodna,trenutna) => prethodna+trenutna,"");
+// console.log(spojBrojeve);
+
+//6.
+
+
+function animal (obj){
+    return "This"+" "+ obj.color+" " + obj.name+" "+"has"+" "+obj.legs+" "+"legs";
+}
+const zivotinja = {
+    name : "dog",
+    color : "white",
+    legs : 4,
+}
+console.log(animal(zivotinja))
