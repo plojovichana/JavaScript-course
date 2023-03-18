9188f12386cd2c016114c387647bb29bb6564214
Hana Plojovic <hanaplojovic123@gmail.com>
2023-03-18 15:52:00 +0100

    JS-course

---
+++ casovi/vezba1-PogodiBroj/script.js
@@ -0,0 +1,19 @@
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+let p = document.querySelector('p');
+let random = Math.floor(Math.random() * 5) + 1;
+let brojac = 0;
+
+button.addEventListener('click', function() {
+    let pogodi = Number(input.value);
+    brojac++;
+    if (pogodi === random) {
+        p.textContent = 'Pogodili ste broj iz ' + brojac +" pokusaja";
+        button.disabled = true;
+    } else if (pogodi < random) {
+        p.textContent = 'Broj treba da bude veci!';
+    } else if (pogodi > random) {
+        p.textContent = 'Broj treba da bude manji!';
+    }
+    }
+);
---
+++ casovi/vezba2-ToDoList/script.js
@@ -0,0 +1,72 @@
+// let card = document.querySelector('.card');
+// let inp = document.querySelector('input');
+// let button = document.querySelector('button');
+
+// console.log(card);
+// card.style.width = '300px';
+// card.style.height = '300px';
+// card.style.backgroundColor = 'red';
+// button.addEventListener('click', ()=>{
+//     let tekst = document.createElement('p');
+//     tekst.innerText = inp.value;
+//     let check = document.createElement('input');
+//     check.type="checkbox";
+//     card.appendChild(tekst);
+//     card.appendChild(check);
+
+//     check.addEventListener('click', ()=>{
+//         if(check.checked){
+//             tekst.style.textDecoration = 'line-through';
+//         }else{
+//             tekst.style.textDecoration = 'none';
+//         }
+//     })
+// })
+
+
+// let card = document.querySelector('.card');
+// let inp = document.querySelector('input');
+// let button = document.querySelector('button');
+
+// button.addEventListener('click', ()=>{
+//     let tekst = document.createElement('p');
+//     tekst.innerText = inp.value;
+//     let check = document.createElement('input');
+//     check.type="checkbox";
+//     card.appendChild(tekst);
+//     card.appendChild(check);
+
+//     check.addEventListener('click', ()=>{
+//         if(check.checked){
+//             tekst.style.textDecoration = 'line-through';
+//         }else{
+//             tekst.style.textDecoration = 'none';
+//         }
+//     })
+// })
+
+let input = document.getElementById("input");
+        let btn = document.getElementById("btn");
+        let list = document.getElementById("list");
+        let arr = [];
+        btn.addEventListener("click", function(){
+            let li = document.createElement("li");
+            li.innerHTML = input.value;
+            let btn = document.createElement("button");
+            btn.innerHTML = "Delete";
+            li.appendChild(btn);
+            list.appendChild(li);
+            arr.push(li);
+            input.value = "";
+            btn.addEventListener("click", function(){
+                li.remove();
+                arr.splice(arr.indexOf(li), 1);
+            })
+
+
+            li.addEventListener("click",()=>{
+                li.style.textDecoration = "line-through";
+            })
+        })
+
+
