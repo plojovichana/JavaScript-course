8617dfbc14b7fa560dfe90b49b86ea37343d9829
Hana Plojovic <hanaplojovic123@gmail.com>
2023-03-25 10:28:14 +0100

    JS-course

--- casovi/vezba2-ToDoList/script.js
+++ casovi/vezba2-ToDoList/script.js
@@ -1,71 +1,32 @@
+let input = document.getElementById("input");
+        let btn = document.getElementById("btn");
+        let list = document.getElementById("list");
+        let arr = [];
+        btn.addEventListener("click", function(){
+            let li = document.createElement("li");
+            li.innerHTML = input.value;
+            let btn = document.createElement("button");
+            btn.innerHTML = "Delete";
+            li.appendChild(btn);
+            list.appendChild(li);
+            arr.push(li);
+            input.value = "";
+            btn.addEventListener("click", function(){
+                li.remove();
+                arr.splice(arr.indexOf(li), 1);
+            })
+            li.addEventListener("click", function(){
+                
+                if(li.style.textDecoration == "line-through"){
+                    li.style.textDecoration = "none";
+                }else{
+                    li.style.textDecoration = "line-through";
+                }
+
+            }
+            )
 
 
 
+        })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//let input = document.getElementById("input");
-//         let btn = document.getElementById("btn");
-//         let list = document.getElementById("list");
-//         let arr = [];
-//         btn.addEventListener("click", function(){
-//             let li = document.createElement("li");
-//             li.innerHTML = input.value;
-//             let btn = document.createElement("button");
-//             btn.innerHTML = "Delete";
-//             li.appendChild(btn);
-//             list.appendChild(li);
-//             arr.push(li);
-//             input.value = "";
-//             btn.addEventListener("click", function(){
-//                 li.remove();
-//                 arr.splice(arr.indexOf(li), 1);
-//             })
-
-
-//             li.addEventListener("click",()=>{
-//                 li.style.textDecoration = "line-through";
-//             })
-//         })
-
-
-
-
-console.log("Hello World");
